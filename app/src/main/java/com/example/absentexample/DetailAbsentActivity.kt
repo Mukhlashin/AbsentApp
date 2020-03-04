7d9ee4b2d39e3c847125dfeb9153d3adbd2fd8e6
Abdullah Mukhlashin <abdullahmukhlashiin@gmail.com>
2020-03-04 13:37:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/absentexample/DetailAbsentActivity.kt
@@ -0,0 +1,12 @@
+package com.example.absentexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailAbsentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_absent)
+    }
+}
